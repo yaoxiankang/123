e90be7efc1ac6a733455c1e3d71de4139cb49062
yaoxiankang <74295384+yaoxiankang@users.noreply.github.com>
2020-11-25 16:51:14 +0800

    Create 112502.QS排名查询.app.c

---
+++ 112502.QS排名查询.app.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main(){
+int i;
+scanf("%d",&i);
+if(i==1)
+printf("清华大学");
+else if(i==2)
+printf("北京大学");
+else if(i==3)
+printf("浙江大学");
+else if(i==4)
+printf("复旦大学");
+else if(i==5)
+printf("中央财经大学");
+else if(i==6)
+printf("同济大学");
+else if(i==7)
+printf("北京外国语大学");
+else if(i==8)
+printf("中山大学");
+else if(i==9)
+printf("四川大学");
+else if(i==10)
+printf("广西大学");
+else
+printf("老铁，排名太靠后了");
+}
