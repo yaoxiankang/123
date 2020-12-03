94350c3c8f4d559d9265cdbfc3ba70123426d887
yaoxiankang <74295384+yaoxiankang@users.noreply.github.com>
2020-12-03 21:04:52 +0800

    Create 120302.遍历二维数组。app.c

---
+++ 120302.遍历二维数组。app.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+    
+  int a[2][4]={
+      {1,2,3,4},
+        {5,6,7,8},
+        
+    };
+   int i,j;
+   for (i=0; i<2; i++) {
+     for (j=0; j<4; j++) {
+         printf("%d,",a[i][j]);
+     }
+      printf("\n");   }
+    return 0;
+}
