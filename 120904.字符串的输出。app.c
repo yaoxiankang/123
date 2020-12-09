3710e37ae37444fc28aa69c1e56d0c79ecfc7c29
yaoxiankang <74295384+yaoxiankang@users.noreply.github.com>
2020-12-09 16:37:25 +0800

    Update 120904.字符串的输出。app.c

--- 120904.字符串的输出。app.c
+++ 120904.字符串的输出。app.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 void main(){
-	char a[]="hello,how  are  you?";
-			char b[]="嗨，你好吗？";
-	printf("%s\n",a);
+    char a[]="hello,how  are  you?";
+		char b[]="嗨，你好吗？";
+    printf("%s\n",a);
 		printf("%s\n",b);
 }
