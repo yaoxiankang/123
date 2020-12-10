19076f0a87bdd4ff3bad0453728d33ae005ff1b9
yaoxiankang <74295384+yaoxiankang@users.noreply.github.com>
2020-12-10 21:22:31 +0800

    Create 121002.猜数字。app.c

---
+++ 121002.猜数字。app.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<string.h>
+void main() {
+	int a=0;
+	int b = 5;
+	char w;
+	printf("想玩儿游戏吗，先回答下一句是什么？天王盖地虎，___________。\n");
+	while
+		scanf("%s", &w);
+		if(!strcmp(!w," 宝塔镇河妖")) {
+			printf("答对了，开始游戏吧!\n");
+		}
+		else
+			printf("答错了，请重答\n");
+	
+	for(;;) {
+		printf("输入在0  ~  20\n");
+		scanf("%d", &a); {
+			printf("请输入数字\n");
+		}
+		if (a == b) {
+			printf("答对了,你太厉害了，答案是%d\n", b);
+			break;
+		} 
+		else if (a > b) {
+			printf("数值大了\n");
+		}
+		else if (a < b) {
+			printf("数值小了\n");
+		}
+	}
+
+}
