55598b540adf895b903e1ee798c11ce99b803076
yaoxiankang <74295384+yaoxiankang@users.noreply.github.com>
2020-12-14 19:43:43 +0800

    Update 121002.猜数字。app.c

--- 121002.猜数字。app.c
+++ 121002.猜数字。app.c
@@ -1,33 +1,25 @@
 #include<stdio.h>
 #include<string.h>
-void main() {
-	int a=0;
-	int b=5;
-	char w[50];
-	printf("想玩儿游戏吗，先回答下一句是什么？天王盖地虎，___________。\n");
-	while(w!=27){
-		scanf("%s", &w);
-		if(!strcmp(w," 宝塔镇河妖")) {
-			printf("答对了，开始游戏吧!\n");
-		}
-		else
-			printf("答错了，请重答\n");
+void main(){
+	int a=1;
+	int b=2;
+	char i[50];
+		printf("想玩儿游戏吗，先回答下一句是什么？天王盖地虎，___________。\n");
+	scanf("%s",&i);
+	if(!strcmp(i,"宝塔镇河妖")){
+		printf("答对了，开始游戏吧!\n");
 	}
-	for(;;) {
-		printf("输入在0  ~  20\n");
-		scanf("%d", &a); {
-			printf("请输入数字\n");
-		}
-		if (a == b) {
-			printf("答对了,你太厉害了，答案是%d\n", b);
-			break;
-		} 
-		else if (a > b) {
-			printf("数值大了\n");
-		}
-		else if (a < b) {
-			printf("数值小了\n");
-		}
+	else
+		printf("答错了，请重答\n");
+	printf("输入在0  ~  20\n");
+	scanf("%s",&a);
+	if(a==b){
+		printf("答对了,你太厉害了，答案是%d\n");
 	}
-
+	else if(a>b){
+		printf("数值大了\n");
+	}
+	else if(a<b){
+		printf("数值小了\n");
+	}
 }
